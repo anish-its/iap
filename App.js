1788b2db061321a6c4f55236613f9826869b77b2
unknown <anish.its@itspectrumsolutions.com>
2023-10-31 19:08:06 +0530

    second push

--- App.js
+++ App.js
@@ -1,58 +1,68 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
+import {View, Text, Button, Platform, Alert} from 'react-native';
 import {
-  View,
-  Text,
-  Button,
-  Platform,
-  Alert,
-} from 'react-native';
-import {initConnection,endConnection,getProducts,buyProduct} from 'react-native-iap';
+  initConnection,
+  endConnection,
+  getProducts,
+  buyProduct,
+} from 'react-native-iap';
 
-function App(){
-  const [products, setProducts] = useState([]);
+function App() {
+    const [products, setProducts] = useState([]);
 
-  const itemSkus = Platform.select({
-    ios: ['com.example.productid'],
-    android: ['com.example.productid'],
-  });
+      const itemSkus = Platform.select({
+        ios: ['android.test.purchase'],
+        android: ['android.test.purchase'],
+      });
 
-  useEffect(() => {
-    initConnection().then(async () => {
-      try {
-        const retrievedProducts = await getProducts(itemSkus);
-        setProducts(retrievedProducts);
-      } catch (error) {
-        console.error(error);
-      }
-    });
+    useEffect(() => {
+        initConnection().then(async () => {
+        try {
+            const retrievedProducts = await getProducts({skus:itemSkus});
+            setProducts(retrievedProducts);
+        } catch (error) {
+            console.error(error);
+        }
+        });
 
-    return () => {
-      endConnection();
-    };
-  }, []);
+        return () => {
+        endConnection();
+        };
+    }, []);
 
-  const handlePurchase = async (sku) => {
-    try {
-      const purchase = await buyProduct(sku);
-      // Handle the successful purchase, unlock content, etc.
-      Alert.alert('Purchase Successful', `You've purchased ${purchase.productId}`);
-    } catch (error) {
-      console.error(error);
-      Alert.alert('Purchase Failed', 'Something went wrong with your purchase.');
-    }
+    const handlePurchase = async (sku) => {
+        try {
+            const purchase = await buyProduct(sku);
+            // Handle the successful purchase, unlock content, etc.
+            Alert.alert(
+                'Purchase Successful',
+                `You've purchased ${purchase.productId}`,
+            );
+            } catch (error) {
+            console.error(error);
+            Alert.alert(
+                'Purchase Failed',
+                'Something went wrong with your purchase.',
+            );
+        }
   };
 
-  return (
-    <View>
-      <Text style={{textAlign:'center'}}>In-App Purchase Example</Text>
-      {products.map((product) => (
-        <View key={product.productId}>
-          <Text>{product.title} - {product.price}</Text>
-          <Button title={`Buy ${product.title}`} onPress={() => handlePurchase(product.productId)}/>
+    return (
+        <View>
+        <Text style={{textAlign: 'center'}}>In-App Purchase Example</Text>
+        {products.map(product => (
+            <View key={product.productId}>
+            <Text>
+                {product.title} - {product.price}
+            </Text>
+            <Button
+                title={`Buy ${product.title}`}
+                onPress={() => handlePurchase(product.productId)}
+            />
+            </View>
+        ))}
         </View>
-      ))}
-    </View>
-  );
-};
+    );
+}
 
 export default App;
